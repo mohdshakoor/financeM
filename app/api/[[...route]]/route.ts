ccdb94557322789f5f8cbdb2ac2b80b5d4d8c659
mohd shakoor <samsuddin.s924@gmail.com>
2025-08-24 16:11:00 +0530

    account-get-api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,17 +1,18 @@
 
 import { Hono } from "hono";
 import {handle} from 'hono/vercel';
-import { clerkMiddleware} from '@hono/clerk-auth';
-import authors from './authors'
-import books from './books'
+
+import accounts from "./accounts";
 
 export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api');
 
-app.get("/hello" ,(c) => {
-    return c.json({hello:"world"});
-})
+
+const routes = app
+ .route("/accounts" , accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;          
