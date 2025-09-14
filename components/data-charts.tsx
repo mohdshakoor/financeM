89db57d7cf12adc4f7bb37ad80cbf43adb50086f
mohd shakoor <samsuddin.s924@gmail.com>
2025-09-14 23:37:02 +0530

    dashboard-pie

--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -1,16 +1,22 @@
 "use client"
 
 import { useGetSummary } from "@/features/summary/api/use-get-summary"
-import { Chart } from "@/components/chart";
+import { Chart, ChartLoading } from "@/components/chart";
+import { SpendingPie, SpendingPieLoading } from "./spending-pie";
 
 export const DataCharts = () => {
     const {data, isLoading} = useGetSummary();
 
     if (isLoading) {
         return (
-            <div>
-                Loading...
-            </div>
+           <div className="grid grid-col-1 lg:grid-cols-6 gap-8">
+                <div className="col-span-1 lg:col-span-3 xl:xol-span-4">
+                    <ChartLoading/>
+                </div>
+                <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+                    <SpendingPieLoading/>
+                </div>
+              </div>
         )
     }
 
@@ -18,6 +24,9 @@ export const DataCharts = () => {
               <div className="grid grid-col-1 lg:grid-cols-6 gap-8">
                 <div className="col-span-1 lg:col-span-3 xl:xol-span-4">
                     <Chart data={data?.days}/>
+                </div>
+                <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+                    <SpendingPie data={data?.categories}/>
                 </div>
               </div>
     )
