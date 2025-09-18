31f0b80ffbd96c02c0b86b515470515b8718a008
mohd shakoor <samsuddin.s924@gmail.com>
2025-09-18 20:29:53 +0530

    Revert "bug-2"
    This reverts commit e3c4a2cbf968693d845bc2ea751a0b8a83867c16.

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,56 +1,54 @@
-// Must be the very first line so Next.js treats this as a client component
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { FaArrowTrendUp, FaArrowTrendDown, FaPiggyBank } from "react-icons/fa6";
+
+import {FaArrowTrendUp,FaArrowTrendDown} from "react-icons/fa6"
 import { formatDateRange } from "@/lib/utils";
 import { useGetSummary } from "@/features/summary/api/use-get-summary";
+import {FaPiggyBank} from "react-icons/fa"
 import { DataCard, DataCardLoading } from "@/components/data-card";
 
 export const DataGrid = () => {
-  // Client-side data fetching
-  const { data, isLoading } = useGetSummary();
+    const {data, isLoading} = useGetSummary();
 
-  // Reading URL query params on the client
-  const params = useSearchParams();
-  const to = params.get("to") || undefined;
-  const from = params.get("from") || undefined;
+    const params = useSearchParams();
+    const to = params.get("to") || undefined;
+    const from = params.get("from") || undefined;
 
-  const dateRangeLabel = formatDateRange({ to, from });
+    const dateRangeLabel = formatDateRange({to, from});
 
-  if (isLoading) {
+    if (isLoading) {
+      return (
+         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+           <DataCardLoading/>
+           <DataCardLoading/>
+           <DataCardLoading/>
+         </div>
+      )
+    }
     return (
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-        <DataCardLoading />
-        <DataCardLoading />
-        <DataCardLoading />
-      </div>
-    );
-  }
-
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-      <DataCard
-        title="Remaining"
-        value={data?.remainingAmount}
-        percentageChange={data?.remainingChange}
-        icon={FaPiggyBank}
-        dateRange={dateRangeLabel}
-      />
-      <DataCard
-        title="Income"
-        value={data?.incomeAmount}
-        percentageChange={data?.incomeChange}
-        icon={FaArrowTrendUp}
-        dateRange={dateRangeLabel}
-      />
-      <DataCard
-        title="Expenses"
-        value={data?.expensesAmount}
-        percentageChange={data?.expensesChange}
-        icon={FaArrowTrendDown}
-        dateRange={dateRangeLabel}
-      />
-    </div>
-  );
-};
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+          <DataCard
+            title="Remaining"
+            value={data?.remainingAmount}
+            percentageChange={data?.remainingChange}
+            icon = {FaPiggyBank}
+            dateRange={dateRangeLabel}
+          />
+          <DataCard
+            title="Income"
+            value={data?.incomeAmount}
+            percentageChange={data?.incomeChange}
+            icon = {FaArrowTrendUp}
+            dateRange={dateRangeLabel}
+          />
+          <DataCard
+            title="Expenses"
+            value={data?.expensesAmount}
+            percentageChange={data?.expensesChange}
+            icon = {FaArrowTrendDown}
+            dateRange={dateRangeLabel}
+          />
+        </div>
+    )
+}
